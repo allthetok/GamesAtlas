16662eb90f415084e653218f5c09233940b406cf
allthetok <al.tokjuman@gmail.com>
2023-08-15 19:53:34 -0400

    adding mock data

---
+++ backendga/mockdata/search.js
@@ -0,0 +1,199 @@
+const search = {
+	"id": 76882,
+	"age_ratings": [
+		23924,
+		24520,
+		91285,
+		91471,
+		145325,
+		145326
+	],
+	"artworks": [
+		7017,
+		7018,
+		7019,
+		7020,
+		7021,
+		7022,
+		7023,
+		7024,
+		7025,
+		7026,
+		7027,
+		7182
+	],
+	"category": 0,
+	"cover": 106347,
+	"external_games": [
+		1027879,
+		1457750,
+		1484040,
+		1593480,
+		1708869,
+		1865696,
+		1865812,
+		1865938,
+		1929043,
+		1931514,
+		1931802,
+		1933412,
+		1934183,
+		1934801,
+		1936165,
+		1936520,
+		1936759,
+		1937946,
+		2085322,
+		2085366,
+		2087343,
+		2088276,
+		2175869,
+		2585232,
+		2585882,
+		2587538,
+		2587981
+	],
+	"first_release_date": 1553212800,
+	"follows": 134,
+	"game_modes": [
+		1
+	],
+	"genres": [
+		25,
+		31
+	],
+	"hypes": 64,
+	"involved_companies": [
+		112950,
+		112951
+	],
+	"keywords": [
+		164,
+		1567,
+		12087,
+		18032,
+		37934,
+		39050
+	],
+	"name": "Sekiro: Shadows Die Twice",
+	"platforms": [
+		6,
+		48,
+		49,
+		170
+	],
+	"player_perspectives": [
+		2
+	],
+	"screenshots": [
+		213852,
+		213853,
+		213854,
+		213855,
+		213856,
+		213857,
+		213858,
+		256125,
+		256126,
+		256127
+	],
+	"similar_games": [
+		19564,
+		27092,
+		27316,
+		54775,
+		55038,
+		55199,
+		81249,
+		96217,
+		103168,
+		106987
+	],
+	"slug": "sekiro-shadows-die-twice",
+	"storyline": "The game takes place in a re-imagined late 16th century Sengoku period Japan. At the end of the Sengoku era, warlord Isshin Ashina stages a bloody coup and seizes the land of Ashina. During this time, a nameless orphan is adopted by the wandering shinobi Owl, who names the orphan \"Wolf\".\n\nTwenty years later, the Ashina clan are on the brink of collapse due to a combination of the now elderly Isshin having fallen ill and the clan's enemies steadily closing in from all sides. Desperate to find a way to prevent the fall of his clan, Isshin's grandson Genichiro captures the Divine Heir Kuro, in hopes of using his \"Dragon Heritage\" to create an immortal army.",
+	"summary": "Enter a dark and brutal new gameplay experience from the creators of Bloodborne and the Dark Souls series. Sekiro: Shadows Die Twice is an intense, third-person, action-adventure set against the bloody backdrop of 14th-century Japan. Step into the role of a disgraced warrior brought back from the brink of death whose mission is to rescue his master and exact revenge on his arch nemesis.\n\nTake Revenge. Restore Your Honor. Kill Ingeniously.",
+	"tags": [
+		1,
+		17,
+		22,
+		23,
+		268435481,
+		268435487,
+		536871076,
+		536872479,
+		536882999,
+		536888944,
+		536908846,
+		536909962
+	],
+	"themes": [
+		1,
+		17,
+		22,
+		23
+	],
+	"total_rating": 90.59219484109542,
+	"total_rating_count": 702,
+	"url": "https://www.igdb.com/games/sekiro-shadows-die-twice",
+	"videos": [
+		22361,
+		22362,
+		22363,
+		22364,
+		24756,
+		25174,
+		25381,
+		25587
+	],
+	"websites": [
+		80128,
+		80129,
+		80130,
+		80131,
+		80132,
+		80133,
+		80134,
+		87964,
+		351783,
+		562685
+	],
+	"language_supports": [
+		185239,
+		185240,
+		185241,
+		185242,
+		185243,
+		185244,
+		185245,
+		185246,
+		185247,
+		185248,
+		185249,
+		185250,
+		185251,
+		185252,
+		185253,
+		185254,
+		185255,
+		185256,
+		185257,
+		185258,
+		185259,
+		185260,
+		185261,
+		185262,
+		185263,
+		185264,
+		185265,
+		185266,
+		185267,
+		185268,
+		185269,
+		185270
+	],
+	"game_localizations": [
+		1879
+	]
+}
+
+export { search }

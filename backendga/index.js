3ce83a5be2890bbb3a0410097a73fc07143d4b08
allthetok <al.tokjuman@gmail.com>
2023-08-14 22:04:38 -0400

    config files for backend

---
+++ backendga/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable prefer-const */
+require('dotenv').config()
+import express, { NextFunction, Request, Response } from 'express'

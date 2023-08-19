d81ca7f4deaddf89307fc5008a2b3415082e4d4d
allthetok <al.tokjuman@gmail.com>
2023-08-18 20:11:58 -0400

    added types and updated import to be used

--- backendga/helpers/requests.ts
+++ backendga/helpers/requests.ts
@@ -1,5 +1,81 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { Request, Response, NextFunction } from 'express'
 require('dotenv').config()
+
+type SearchConfig = {
+	method: string,
+	url: string,
+	headers: object,
+	data: string
+}
+
+type GameDetailObj = {
+	id: number | null,
+	age_ratings: string | AgeRatings,
+	artworks: string | string[],
+	external_games: string | Categories[],
+	game_modes: string,
+	genres: string | string[],
+	hypes: number | null,
+	involved_companies: string | Companies[],
+	keywords: string | string[],
+	platforms: string | Platforms[],
+	player_perspectives: string,
+	screenshots: string | string[],
+	similar_games: string | string[],
+	tags: string,
+	themes: string | string[],
+	videos: string | Videos[],
+	websites: string | Categories[],
+	language_supports: string | Languages[],
+	game_localizations: string,
+	rating: number | null,
+	ratingCount: number | null,
+	releaseDate: Date | null,
+	likes: number | null,
+	title: string,
+	story: string,
+	summary: string,
+	url: string
+}
+
+type AgeRatings = {
+	'ESRB': number,
+	'PEGI': number
+}
+
+type Categories = {
+	'category': number,
+	'url': string
+}
+
+type Companies = {
+	'name': string,
+	'logoid': number,
+	'url': string
+}
+
+type Platforms = {
+	'name': string,
+	'category': number,
+	'platform_logo': number,
+	'url': string
+}
+
+type Videos = {
+	'name': string,
+	'ytlink': string
+}
+
+type Languages = {
+	'language': string,
+	'language_support_type': string,
+	'marked': boolean,
+	'locale': string,
+	'native': string
+}
+
 
 
 const requestLogger = (request: Request, response: Response, next: NextFunction): void => {
@@ -16,15 +92,8 @@ const corsOptions = {
 	optionSuccessStatus: 200
 }
 
-type SearchConfig = {
-	method: string,
-	url: string,
-	headers: object,
-	data: string
-} 
-
-const updateIGDBSearchConfig = (method: string, endpoint: string, datafields: string, stringofids: string, additionalfilter: string, search: boolean, searchterm: string, limit: number): SearchConfig => {
-	let searchConfig: SearchConfig = {
+const updateIGDBSearchConfig = (method: string, endpoint: string, datafields: string, stringofids: any, additionalfilter: string, search: boolean, searchterm: string, limit: number): SearchConfig => {
+	const searchConfig: SearchConfig = {
 		method,
 		url: `${process.env.API_ROOT_URL}${endpoint}`,
 		headers: {
@@ -47,7 +116,7 @@ const updateIGDBSearchConfig = (method: string, endpoint: string, datafields: st
 	// 	data: ''
 	// }
 	if (search) {
-		searchConfig.data = `search "${searchterm}"; fields ${datafields}; ${limit !== 0 ? ` limit ${limit};` : null}`
+		searchConfig.data = `search "${searchterm}"; fields ${datafields}; ${limit !== 0 ? ` limit ${limit};` : ''}`
 	}
 	else {
 		searchConfig.data = `fields ${datafields}; where id=(${stringofids}) ${additionalfilter !== '' ? ` & ${additionalfilter}` : ''};`
@@ -66,4 +135,4 @@ const updateIGDBSearchConfig = (method: string, endpoint: string, datafields: st
 	return searchConfig
 }
 
-export { requestLogger, corsOptions, updateIGDBSearchConfig, SearchConfig }
+export { requestLogger, corsOptions, updateIGDBSearchConfig, SearchConfig, GameDetailObj, AgeRatings, Categories, Companies, Platforms, Videos, Languages  }

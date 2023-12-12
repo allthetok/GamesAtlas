fd5ba4b104a000e87798ffbcd979affebb3717e1
allthetok <al.tokjuman@gmail.com>
2023-12-11 19:04:33 -0500

    update cors origin

--- backendga/helpers/requests.ts
+++ backendga/helpers/requests.ts
@@ -19,7 +19,7 @@ const requestLogger = (request: Request, response: Response, next: NextFunction)
 }
 
 const corsOptions = {
-	origin: 'http://localhost:3000',
+	// origin: 'http://localhost:3000',
 	credentials: true,	//access-control-allow-credentials:true
 	optionSuccessStatus: 200
 }
